36df3c39dff2d4dbb01ffdd12876dc39167603b9
shizuka-21 <159708705+shizuka-21@users.noreply.github.com>
2024-02-11 21:07:44 +0530

    Add files via upload

---
+++ DbUtilis.java
@@ -0,0 +1,13 @@
+package employee.management;
+
+import java.sql.ResultSet;
+
+import javax.swing.table.TableModel;
+
+public class DbUtilis {
+
+    public static TableModel resultSetToTableModel(ResultSet rs) {
+        return null;
+    }
+
+}
